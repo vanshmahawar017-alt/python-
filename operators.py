eb6099075bb053969f179611421f908cf7b2c78a
Vansh <Vanshmahawar017@gmail.com>
2026-06-23 18:18:03 +0530

    done my work

---
+++ operators.py
@@ -0,0 +1,81 @@
+#Arithmetic Operator
+#+,-,,/,//,%,*
+
+a = 10
+b = 20
+
+#Addition
+c = a+b
+print(c)
+
+#Subtraction
+d = a-b
+print(d)
+
+#Multiplication
+e = a*b
+print(e)
+
+#Division
+f = a/b
+print(f)
+
+#Floor division
+x=15
+y=10
+print(x/y)
+print(x//y)
+
+#Modulo operator
+#Remainder
+print(x%y)
+
+
+#Exponential
+#2^3
+a=2
+b=3
+print(a ** b)
+
+
+#comparion operator
+#compersion operators gives the bolean value True or False
+#==,!=,>,<,>=,<=
+#less than '<'
+a=10
+b=20
+print(a<b)
+#grater than '>'
+print(a>b)
+
+#less than or equal to '<='
+a=10
+b=10
+print(a<=b)
+#grater than or equal to '>='
+c=10
+d=12
+print(c>=d)
+
+#not equal to '!='
+a=1
+b=5
+print(a!=b)
+
+#eqaual to equals '=='
+a=10
+b=10
+print(a==b)
+
+#logical operator
+#and,or,not
+#10 ->9->20
+print((10>9) and (10<20))
+#or
+print((10>a) or (a<20))
+#not
+print(not(11>a))
+
+#membership operator
+#operator  precedence and associativity
+
