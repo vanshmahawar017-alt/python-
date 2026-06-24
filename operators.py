ddbc82f7e40f73afe2cf6bc455bc21741e3b2279
Vansh <Vanshmahawar017@gmail.com>
2026-06-24 18:11:26 +0530

    done my work

--- operators.py
+++ operators.py
@@ -1,81 +1,118 @@
-#Arithmetic Operator
-#+,-,,/,//,%,*
+# #Arithmetic Operator
+# #+,-,,/,//,%,*
 
-a = 10
-b = 20
+# a = 10
+# b = 20
 
-#Addition
-c = a+b
-print(c)
+# #Addition
+# c = a+b
+# print(c)
 
-#Subtraction
-d = a-b
-print(d)
+# #Subtraction
+# d = a-b
+# print(d)
 
-#Multiplication
-e = a*b
-print(e)
+# #Multiplication
+# e = a*b
+# print(e)
 
-#Division
-f = a/b
-print(f)
+# #Division
+# f = a/b
+# print(f)
 
-#Floor division
-x=15
-y=10
-print(x/y)
-print(x//y)
+# #Floor division
+# x=15
+# y=10
+# print(x/y)
+# print(x//y)
 
-#Modulo operator
-#Remainder
-print(x%y)
-
-
-#Exponential
-#2^3
-a=2
-b=3
-print(a ** b)
+# #Modulo operator
+# #Remainder
+# print(x%y)
 
 
-#comparion operator
-#compersion operators gives the bolean value True or False
-#==,!=,>,<,>=,<=
-#less than '<'
-a=10
-b=20
-print(a<b)
-#grater than '>'
-print(a>b)
+# #Exponential
+# #2^3
+# a=2
+# b=3
+# print(a ** b)
 
-#less than or equal to '<='
-a=10
-b=10
-print(a<=b)
-#grater than or equal to '>='
-c=10
-d=12
-print(c>=d)
 
-#not equal to '!='
-a=1
-b=5
-print(a!=b)
+# #comparion operator
+# #compersion operators gives the bolean value True or False
+# #==,!=,>,<,>=,<=
+# #less than '<'
+# a=10
+# b=20
+# print(a<b)
+# #grater than '>'
+# print(a>b)
 
-#eqaual to equals '=='
-a=10
-b=10
-print(a==b)
+# #less than or equal to '<='
+# a=10
+# b=10
+# print(a<=b)
+# #grater than or equal to '>='
+# c=10
+# d=12
+# print(c>=d)
 
-#logical operator
-#and,or,not
-#10 ->9->20
-print((10>9) and (10<20))
-#or
-print((10>a) or (a<20))
-#not
-print(not(11>a))
+# #not equal to '!='
+# a=1
+# b=5
+# print(a!=b)
 
-#membership operator
-#operator  precedence and associativity
+# #eqaual to equals '=='
+# a=10
+# b=10
+# print(a==b)
+
+# #logical operator
+# #and,or,not
+# #10 ->9->20
+# print((10>9) and (10<20))
+# #or
+# print((10>a) or (a<20))
+# #not
+# print(not(11>a))
+
+# #membership operator
+# #operator  precedence and associativity
+
+
+#swap two no. 
+# a=10
+# b=20
+# temp=a
+# a=b
+# b=temp
+# # print(a,b)
+
+# #membership operator
+# #in,not in
+# # s="String"
+# # print('i' in s)
+
+
+# #bitwise operator
+# #&,|,^,~,<<,>>
+# a=5 # 1 0 1
+# b=4 #1 0 0
+# #print(a|b)
+# print(a&b)#and
+# a=5 # 1 0 1
+# b=4 # 1 0 0
+#     # 0 0 1
+# print(a^b)#xor same pr 0 baki pr 1 
+# print(~a)#not
+
+#bitwise xor shift
+# a=5 # 1 0 1
+# b=4 # 1 0 0
+# print(a<<1)#left shift
+
+#right shift
+a=5
+b=4
+print(a>>1)
 
